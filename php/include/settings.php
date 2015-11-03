3829e059eb853d31cb0803021a28b37734cffbb6
Plamen Georgiev <Georgiev@maxds.eu>
2015-11-03 19:26:24 +0200

    Added all files

---
+++ php/include/settings.php
@@ -0,0 +1,20 @@
+<?php
+
+	// MailChimp
+	$listID = "LIST-ID";
+	$APIKey = "API-KEY";
+
+	// Database
+	$host = 'localhost';
+	$username = 'ing';
+	$password = 'iraq2009';
+	$database = 'seventyTwo';
+
+	// Contact
+	$to = 'Georgiev@maxds.eu';
+    $subject = 'testing data';
+
+	// Variant
+	$useMailChimp = "NO";
+
+?>
